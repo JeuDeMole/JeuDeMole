f3d8aa03342fbd7b0b1621d484210ff847a7c201
Emilien Dubois <44874948+MiIou@users.noreply.github.com>
2019-01-03 09:03:06 +0100

    Update Pretre.java

--- JeuDeMole/src/Modele/Pretre.java
+++ JeuDeMole/src/Modele/Pretre.java
@@ -7,9 +7,46 @@ public class Pretre extends PJ {
 		System.out.println("A new chalenger approaches:"+this.getNom()+"\n");
 	}
 	
+	//Bénédictions
+	public void benediction(PJ recevant)
+	{
+		System.out.println(this.getNom + " entonne un chant de guerre et se motive lui et tout ses alliés");
+		recevant.setForce(recevant.getForce() + 1);
+		recevant.setSagesse(recevant.getSagesse() + 1);
+		this.setForce(this.getForce() + 1);
+		this.setSagesse(this.getSagesse() + 1);
+
+	}
+	public void finBenediction(PJ recevant)
+	{
+		recevant.setForce(recevant.getForce() - 1);
+		recevant.setSagesse(recevant.getSagesse() - 1);
+		this.setForce(this.getForce() - 1);
+		this.setSagesse(this.getSagesse() - 1);
+	}
+	
+	//Bouclier de la foi
+	public void BouclierDeLaFoi()
+	{
+		System.out.println("Vous inscrivez le symbole de votre foi sur votre bouclier afin d'augmenter sa résistance");
+		this.setArmure(this.getArmure() + 2);
+	}
+	
+	//Soin Léger
+	public void soinLeger(PJ recevant)
+	{
+		System.out.println("Vous poser vos mains sur la blessure et vous sentez les plaies se guerir sous vos doigts");
+		recevant.setPV(recevant.getPV() + this.getSagesse());
+		if (recevant.getPV() > recevant.getPVMax())
+		{
+			recevant.setPV(recevant.getPVmax());
+		}
+	}
+
 	@Override
 	public String getEntete(){
 		return ("Pretre");
 }
+	
 
 }
