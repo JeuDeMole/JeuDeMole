f931c452d4fd5555a8cccdf58d22255898684d83
Emilien Dubois <44874948+MiIou@users.noreply.github.com>
2019-01-03 09:05:05 +0100

    Update Guerrier.java

--- JeuDeMole/src/Modele/Guerrier.java
+++ JeuDeMole/src/Modele/Guerrier.java
@@ -7,7 +7,28 @@ public class Guerrier extends PJ {
 		super(nom, pv, lvl, armure, initiative, po, degats, force, sagesse);
 		System.out.println("A new chalenger approaches:"+this.getNom()+"\n");
 	}
-
+	
+	//attaque double
+	public void attaqueDouble(PersoAction recevant)
+	{
+		System.out.println(this.getNom() + " fait une double attaque !!");
+		for(int i =0; i<2;i++)
+		{
+			this.attack(recevant);
+		}
+	}
+	//Armure naturelle
+	public void armureNaturelle()
+	{
+		System.out.println("Vos reflexes deviennent plus vifs et vous confèrent une meilleure defense en combat");
+		this.setArmure(this.getArmure() + 2);
+	}
+	//Science du critique
+	public void scienceDuCritique()
+	{
+		System.out.println("Votre precision s'accrue ce qui vous permet d'augmenter vos chances des faire des degats critique");
+		this.setCritique(19);
+	}
 	@Override
 	public String getEntete(){
 		return ("Guerrier");
